74ff18b2d758be5caee3a9ad6a4d2d97df2a50d5
bareselfliege <bareselfliege@gmail.com>
2025-03-16 21:06:36 +0000

    Add 11.php

---
+++ src/11.php
@@ -0,0 +1,10 @@
+<?php
+// Function to calculate the area of a rectangle
+function getArea($length, $width) {
+  return $length * $width;
+}
+
+// Test the function with different values
+echo getArea(5, 3); // Should print "15"
+echo getArea(2, 4); // Should print "8"
+?>
