34bbb00143b8c0011423d1beb815247638d71845
bareselfliege <bareselfliege@gmail.com>
2025-03-24 06:14:00 +0000

    Add 18.php

---
+++ src/18.php
@@ -0,0 +1,6 @@
+<?php
+// Example PHP code for Math Homework Assignment
+
+echo "Hello, this is an example of a basic PHP script.\n";
+echo "It contains some variables and simple calculations.";
+?>
