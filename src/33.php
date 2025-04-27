1f9161edc3c42b5234b5bd8a4662cb157a8e6b4a
bareselfliege <bareselfliege@gmail.com>
2025-04-27 02:14:58 +0000

    Add 33.php

---
+++ src/33.php
@@ -0,0 +1,9 @@
+<?php
+// Define an array of numbers
+$numbers = [10, 20, 30, 40, 50];
+
+// Loop through the array and print each number
+foreach ($numbers as $number) {
+    echo $number . PHP_EOL;
+}
+?>
